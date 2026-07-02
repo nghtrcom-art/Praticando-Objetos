4e8ae5a3a8bed908f4a2992c44ab4800dfdad58a
nghtrcom-art <nghtr.com@gmail.com>
2026-07-02 19:54:07 -0300

    cadastra e exibe modelos de carro

--- src/Algoritmo.java
+++ src/Algoritmo.java
@@ -2,17 +2,18 @@ import com.Carro.entity.Carro;
 import java.util.Scanner;
 public class Algoritmo {
 
-    public static void main(String[] args){
+    static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite a quantidade de carros que deseja cadastrar (máximo de 50):\n");
         int quantCar = 0;
         quantCar = sc.nextInt();
-        Carro carros[] = new Carro[quantCar];
+        Carro[] carros = new Carro[quantCar];
 
         for (int i = 0; i < quantCar; i++) {
 
+            sc.nextLine();
             System.out.println("Modelo " + (i + 1));
             System.out.println("Digete o modelo do carro:");
             String modelo = sc.nextLine();
